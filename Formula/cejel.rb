acc7423af500c69f9f7591bf02cec13fc307257e
Houman <houman3@gmail.com>
2026-07-24 04:21:33 -0700

    cejel: update to v0.1.9 (#5)
    * cejel: update to v0.1.9
    
    * ci: validate tap changes with brew test-bot

--- Formula/cejel.rb
+++ Formula/cejel.rb
@@ -1,30 +1,30 @@
 class Cejel < Formula
   desc "Offline engineering-trust certificate for codebases"
   homepage "https://cejel.dev"
-  version "0.1.8"
+  version "0.1.9"
   license "AGPL-3.0-only"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/BargLabs/cejel/releases/download/v0.1.8/cejel-Darwin-arm64",
+      url "https://github.com/BargLabs/cejel/releases/download/v0.1.9/cejel-Darwin-arm64",
           using: :nounzip
-      sha256 "80ea2bd3060b5d2442aad38e662ec6ec5bf8cec1cb97f9fd22dd4fd73f0279fe"
+      sha256 "f00bc6aa557ead97fe1409b0cbb9327982ceba9173502acc172ea8bce0007256"
     else
-      url "https://github.com/BargLabs/cejel/releases/download/v0.1.8/cejel-Darwin-x86_64",
+      url "https://github.com/BargLabs/cejel/releases/download/v0.1.9/cejel-Darwin-x86_64",
           using: :nounzip
-      sha256 "3c9a7be5dd137354121fe19e889fad7c8ce5d7b11a694a825d9319cfc9b4f3ab"
+      sha256 "51b815fa3a49d25bd6e62f0853771b0d22519b7ebc3506c5af0d4c091bb91829"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/BargLabs/cejel/releases/download/v0.1.8/cejel-Linux-aarch64",
+      url "https://github.com/BargLabs/cejel/releases/download/v0.1.9/cejel-Linux-aarch64",
           using: :nounzip
-      sha256 "428c4632ba856445c1919dfc8324a7b8d593cc6f1f93da8f0ccedb954635af2a"
+      sha256 "7305866ccb202068d0d8e95d56200ed84b9398bf234701ff454e725c96bfe0b3"
     else
-      url "https://github.com/BargLabs/cejel/releases/download/v0.1.8/cejel-Linux-x86_64",
+      url "https://github.com/BargLabs/cejel/releases/download/v0.1.9/cejel-Linux-x86_64",
           using: :nounzip
-      sha256 "1070cd200edaefe924accb9fa9402486820447d7b937a860d12f7c85df5715cc"
+      sha256 "13a064fc84477ac8993769f3cdde493b6b89512f6bd8e0868e4a645ea3389515"
     end
   end
 
